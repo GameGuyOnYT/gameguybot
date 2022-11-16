ce3670c91166571065da601e219385d92216f7b4
GameGuy <efeoyun@tahca.com>
2022-11-16 23:50:49 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,54 +1,73 @@
-import json, discord, requests
-import os
+import datetime, discord, json, requests
+from discord.ext import commands, tasks
 
-os.environ['TOKEN'] = 'LOLMTAwOTU3MTY1NzUzNjUxNjA5Ng.G9HVU0.kHfcG34SZIImap-D2YDiCgwc6VknrlRuNLRXeULOL'.replace("LOL", "")
-token = os.environ.get("TOKEN")
+bot = commands.Bot(intents=discord.Intents.all(), command_prefix="!")
+token = 'placeholderMTAwOTU3MTY1NzUzNjUxNjA5Ng.G9HVU0.kHfcG34SZIImap-D2YDiCgwc6VknrlRuNLRXeUplaceholder'.replace("placeholder", "")
 
-client = discord.Client(intents=discord.Intents.default())
-
-
-@client.event
+@bot.listen()
 async def on_ready():
-    print("Connected!")
+    task_loop.start()  # important to start the loop
 
-
-@client.event
-async def on_message(message):
-    if message.author == client.user: return
-    if message.content == '!help':
-        await message.author.send(
-            '```!status - Check if a ROBLOX exploit is online. eg: !status comet, !status wrd...\n!help - Show this message.```')
+@tasks.loop(seconds=10)
+async def task_loop():
+    print("loop point")
     content = json.loads(requests.get("https://api.whatexploitsare.online/status").text)
-    embed_status = discord.Embed(colour=discord.Colour.red(), title="Exploit Status")
-    if message.content == '!status wrd' or message.content == '!status WeAreDevs_API' or message.content == '!status wearedevs_api' or message.content == '!status wearedevs_api' or message.content == '!status wearedevs' or message.content == '!status WeAreDevs': embed_status.description = f'WeAreDevs API / JJSploit: {content[6]["WeAreDevs API"]["updated"]} (version: {content[6]["WeAreDevs API"]["exploit_version"]})'
-    if message.content == '!status temple': embed_status.description = f'Temple: {content[3]["Temple"]["updated"]} (version: {content[3]["Temple"]["exploit_version"]})'
-    if message.content == '!status electron': embed_status.description = f'Electron: {content[5]["Electron"]["updated"]} (version: {content[5]["Electron"]["exploit_version"]})'
-    if message.content == '!status dx9ware': embed_status.description = f'DX9WARE: {content[4]["DX9WARE"]["updated"]} (version: {content[4]["DX9WARE"]["exploit_version"]})'
-    if message.content == '!status celestial': embed_status.description = f'Celestial: {content[10]["Celestial"]["updated"]} (version: {content[10]["Celestial"]["exploit_version"]})'
-    if message.content == '!status comet': embed_status.description = f'Comet: {content[11]["Comet"]["updated"]} (version: {content[11]["Comet"]["exploit_version"]})'
-    if message.content == '!status synapse' or message.content == '!status synapse_x' or message.content == '!status syn' or message.content == '!status syn_x': embed_status.description = f'Synapse:                  {content[0]["Synapse"]["updated"]} (version: {content[0]["Synapse"]["exploit_version"]})'
-    if message.content == '!status scriptware' or message.content == '!status script_ware' or message.content == '!status sw': embed_status.description = f'Script-Ware: {content[1]["Script-Ware"]["updated"]} (version: {content[1]["Script-Ware"]["exploit_version"]})'
-    if message.content == '!status roware' or message.content == '!status ro-ware': embed_status.description = f'Ro-Ware: {content[9]["Ro-Ware"]["updated"]} (version: {content[9]["Ro-Ware"]["exploit_version"]})'
-    if message.content == '!status krnl' or message.content == '!status kernel': embed_status.description = f'KRNL: {content[2]["KRNL"]["updated"]} (version: {content[2]["KRNL"]["exploit_version"]})'
-    if message.content == '!status oxygen' or message.content == '!status oxygen_u' or message.content == '!status oxy_u' or message.content == '!status oxygenu' or message.content == '!status oxyu': embed_status.description = f'Oxygen U: {content[7]["Oxygen"]["updated"]} (version: {content[7]["Oxygen"]["exploit_version"]})'
-    if message.content == '!status fluxus' or message.content == '!status fluxware': embed_status.description = f'Fluxus: {content[8]["Fluxus"]["updated"]} (version: {content[8]["Fluxus"]["exploit_version"]})'
-    if message.content == '!status aliegexecutor' or message.content == '!status Aliegexecutor' or message.content == '!status AliEgexecutor' or message.content == '!status AliEgExecutor': embed_status.description = 'AliEgExecutor: False (version: 1.0)'
-    if message.content == '!status': embed_status.description = (
-        f'Synapse: {content[0]["Synapse"]["updated"]} (version: {content[0]["Synapse"]["exploit_version"]})\n \
-            Script-Ware: {content[1]["Script-Ware"]["updated"]} (version: {content[1]["Script-Ware"]["exploit_version"]})\n \
-            KRNL: {content[2]["KRNL"]["updated"]} (version: {content[2]["KRNL"]["exploit_version"]})\n \
-            Temple: {content[3]["Temple"]["updated"]} (version: {content[3]["Temple"]["exploit_version"]})\n \
-            DX9WARE: {content[4]["DX9WARE"]["updated"]} (version: {content[4]["DX9WARE"]["exploit_version"]})\n \
-            Electron: {content[5]["Electron"]["updated"]} (version: {content[5]["Electron"]["exploit_version"]})\n \
-            WeAreDevs API / JJSploit: {content[6]["WeAreDevs API"]["updated"]} (version: {content[6]["WeAreDevs API"]["exploit_version"]})\n \
-            Oxygen U: {content[7]["Oxygen"]["updated"]} (version: {content[7]["Oxygen"]["exploit_version"]})\n \
-            Fluxus: {content[8]["Fluxus"]["updated"]} (version: {content[8]["Fluxus"]["exploit_version"]})\n \
-            Ro-Ware: {content[9]["Ro-Ware"]["updated"]} (version: {content[9]["Ro-Ware"]["exploit_version"]})\n \
-            Celestial: {content[10]["Celestial"]["updated"]} (version: {content[10]["Celestial"]["exploit_version"]})\n \
-            Comet: {content[11]["Comet"]["updated"]} (version: {content[11]["Comet"]["exploit_version"]})\n \
-            AliEgExecutor: False (version: 1.0)')
-    if message.content.startswith('!status'):
-        await message.author.send(embed=embed_status)
 
+    def onlineCheck(num, exploit):
+        if type(exploit) != str:
+            return -1
+        if type(num) != int:
+            return -1
+        isOnline = content[num][exploit]["updated"]
+        if isOnline == True:
+            isOnline = ":green_circle:"
+        elif isOnline == False:
+            isOnline = ":red_circle:"
+        elif isOnline == 3:
+            isOnline = ":yellow_circle:"
+        elif isOnline == 4:
+            isOnline = ":orange_circle:"
+        return isOnline
 
-client.run(token)
+    def versionCheck(num, exploit):
+        if type(exploit) != str:
+            return -1
+        if type(num) != int:
+            return -1
+        version = content[num][exploit]['exploit_version']
+        version = version.replace(" Belgium Tax Evasion", "")
+        version = version.replace("V", "")
+        version = version.replace("v", "")
+        return version
+
+    def timeCheck(num, exploit):
+        if type(exploit) != str:
+            return -1
+        if type(num) != int:
+            return -1
+        updateTime = content[num][exploit]['last_update_unix']
+        timeConverted = datetime.datetime.utcfromtimestamp(updateTime).strftime('**%m-%d-%Y** at **%H:%M:%S**')
+        return timeConverted.encode("unicode_escape").decode("utf-8")
+
+    embed_status = discord.Embed(colour=discord.Colour.yellow(), title="Exploit Status")
+    guild = bot.get_guild(1039596383717568512)
+    channel = guild.get_channel(1039596384195715127)
+    msg = await channel.fetch_message(1039606848661245992)
+    statuses = (
+        f":green_circle: : **Working** | :red_circle: : **Not Working** | :orange_circle: : **In Testing** | :yellow_circle: : **API / Website Down**\n \n \
+                **Synapse**:          {onlineCheck(0, 'Synapse')} (version: **{versionCheck(0, 'Synapse')}** | last updated on {timeCheck(0, 'Synapse')})\n \
+                **Script-Ware**: {onlineCheck(1, 'Script-Ware')} (version: **{versionCheck(1, 'Script-Ware')}** | last updated on {timeCheck(1, 'Script-Ware')})\n \
+                **KRNL**:                 {onlineCheck(2, 'KRNL')} (version: **{versionCheck(2, 'KRNL')}** | last updated on {timeCheck(2, 'KRNL')})\n \
+                **DX9WARE**:      {onlineCheck(3, 'DX9WARE')} (version: **{versionCheck(3, 'DX9WARE')}** | last updated on {timeCheck(3, 'DX9WARE')})\n \
+                **Electron**:          {onlineCheck(4, 'Electron')} (version: **{versionCheck(4, 'Electron')}** | last updated on {timeCheck(4, 'Electron')})\n \
+                **JJSploit**:          {onlineCheck(5, 'WeAreDevs API')} (version: **{versionCheck(5, 'WeAreDevs API')}** | last updated on {timeCheck(5, 'WeAreDevs API')})\n \
+                **Oxygen U**:       {onlineCheck(6, 'Oxygen')} (version: **{versionCheck(6, 'Oxygen')}** | last updated on {timeCheck(6, 'Oxygen')})\n \
+                **Fluxus**:              {onlineCheck(7, 'Fluxus')} (version: **{versionCheck(7, 'Fluxus')}** | last updated on {timeCheck(7, 'Fluxus')})\n \
+                **Ro-Ware**:         {onlineCheck(8, 'Ro-Ware')} (version: **{versionCheck(8, 'Ro-Ware')}** | last updated on {timeCheck(8, 'Ro-Ware')})\n \
+                **Celestial**:         {onlineCheck(9, 'Celestial')} (version: **{versionCheck(9, 'Celestial')}** | last updated on {timeCheck(9, 'Celestial')})\n \
+                **Comet**:              {onlineCheck(10, 'Comet')} (version: **{versionCheck(10, 'Comet')}** | last updated on {timeCheck(10, 'Comet')})")
+    embed_status.description = statuses
+
+    await msg.edit(embed=embed_status)
+
+bot.run(token)
